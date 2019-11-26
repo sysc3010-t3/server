9059159439d65e5fb43b9f7a31bb13b48f596dc9
Igor Veselinovic <igor@veselinovic.net>
2019-11-26 14:16:44 -0500

    Add handler for CONN_CAR
    Still need to figure out how to deal with a car that is already ON.
    Might not need to do anything differently, or maybe should add a check
    and then send an ACK or ERROR in response if it's already ON.

--- main.py
+++ main.py
@@ -16,4 +16,5 @@ if __name__ == '__main__':
     server.add_handler(MsgType.REG_USER, handlers.handle_register_user)
     server.add_handler(MsgType.REG_CAR, handlers.handle_register_car)
     server.add_handler(MsgType.LOGIN, handlers.handle_login)
+    server.add_handler(MsgType.CONN_CAR, handlers.handle_connect_car)
     server.add_handler(MsgType.MOVE, handlers.handle_movement)
